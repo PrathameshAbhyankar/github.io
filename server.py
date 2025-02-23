2c3f7696c8bd3568d5961ef3521ff734abed4b1d
Pranav Rakh <pranavrakh@gmail.com>
2025-02-23 20:20:13 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -177,25 +177,18 @@ def cloudData(argsCloud,arg1):
         name = "Bobby" + str(it)
         it = it + 1    
         data = json.dumps({
-	    'timestamp': timestamp_new,
-	    'transaction_id': '8d1e1533-6071-4b10-9cda-b8429c1c7a67',
-	    'name': name,
-	    'email': 'bobby.drake@pressure.io',
-	    'age': 42,
-	    'passport_number': 3847665,
-	    'flight_from': 'Barcelona',
-	    'flight_to': 'London',
-	    'extra_bags': 1,
-	    'flight_class': arrayAnimal[0],
-	    'priority_boarding': False,
-	    'meal_choice': 'vegetarian',
-	    'seat_number': '15D',
-	    'airline': 'Red Balloon'
+	    'time_observed': arrayTime[0],
+	    'location': arrayLocation[0],
+	    'count': arrayNode[0],
+	    'species': arrayAnimal[0],
+	    'temperature': str(Fire_Reported),
+	    'humidity':str(Fire_Reported),
+	    'observer_id':name	    
 	})
 
         r = requests.post('https://api.us-east.tinybird.co/v0/events', 
         params = {
-	    'name': 'project_specific',
+	    'name': 'demo_custom_data_5038',
 	    'token': 'p.eyJ1IjogImM1YmRhY2YxLTI5YzUtNDIwZS1hYzMxLTY2OWJmMjAzZjBlZCIsICJpZCI6ICIwMmFiZWM0ZC0xM2VkLTRhYzItYmE1OS1iYzQ3YTE4ZTRlZjMiLCAiaG9zdCI6ICJ1c19lYXN0In0.vuZo6A9d9-gzyqQ0ftGBeJjvyfkzwilUqsIU2T8zWYE',
 	}, 
 	data=data)
