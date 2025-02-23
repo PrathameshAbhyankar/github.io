a9f2bdd5982b394616d1ad0038a1cff91d400b61
Pranav Rakh <pranavrakh@gmail.com>
2025-02-23 19:44:17 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -14,9 +14,12 @@ import threading
 
 import socket             
 
-from flask import Flask, render_template
+#from flask import Flask, render_template
 #import flask
 import datetime
+
+import requests
+import json
 
 import geocoder
 from geopy.geocoders import Nominatim
@@ -97,7 +100,7 @@ def getObjects(img, thres, nms, draw=True, objects=[]):
                     j += 1;
                     if j > 10 : 
                        j=10;
-                    #print(ObjectName)
+                    print(ObjectName)
                     #print("The variable, name is of type:", type(classNames[classId-1].upper()))                    
 
 
@@ -161,8 +164,45 @@ def frameCapture(clientData,a,b,c,d):
         cv2.imshow("Output",img)
         cv2.waitKey(1)
  
+argsCloud = "CD"
 
+def cloudData(argsCloud,arg1):
 
+    it = 1
+    while True:  
+
+       
+        now = datetime.datetime.now()
+        timestamp_new = now.strftime("%Y-%m-%d %H:%M:%S")
+        name = "Bobby" + str(it)
+        it = it + 1    
+        data = json.dumps({
+	    'timestamp': timestamp_new,
+	    'transaction_id': '8d1e1533-6071-4b10-9cda-b8429c1c7a67',
+	    'name': name,
+	    'email': 'bobby.drake@pressure.io',
+	    'age': 42,
+	    'passport_number': 3847665,
+	    'flight_from': 'Barcelona',
+	    'flight_to': 'London',
+	    'extra_bags': 1,
+	    'flight_class': arrayAnimal[0],
+	    'priority_boarding': False,
+	    'meal_choice': 'vegetarian',
+	    'seat_number': '15D',
+	    'airline': 'Red Balloon'
+	})
+
+        r = requests.post('https://api.us-east.tinybird.co/v0/events', 
+        params = {
+	    'name': 'project_specific',
+	    'token': 'p.eyJ1IjogImM1YmRhY2YxLTI5YzUtNDIwZS1hYzMxLTY2OWJmMjAzZjBlZCIsICJpZCI6ICIwMmFiZWM0ZC0xM2VkLTRhYzItYmE1OS1iYzQ3YTE4ZTRlZjMiLCAiaG9zdCI6ICJ1c19lYXN0In0.vuZo6A9d9-gzyqQ0ftGBeJjvyfkzwilUqsIU2T8zWYE',
+	}, 
+	data=data)
+
+        print(r.status_code)
+        print(r.text)
+        time.sleep(1)        	
 
 
 def listener(client, address):
@@ -212,4 +252,6 @@ while True:
     th.append(Thread(target=listener, args = (client,address)).start())
     
     th.append(Thread(target=frameCapture, args = (ObjectName)).start())    
+    th.append(Thread(target=cloudData, args = (argsCloud)).start())        
+    
 #s.close()
